df463c61debbd5f7f957e39b2ba36003cfbc8cde
PingMaster99 <59905458+PingMaster99@users.noreply.github.com>
2020-05-30 14:02:31 -0600

    Functions separated into different files / Getting user preferences

--- GenerateDatabase.py
+++ GenerateDatabase.py
@@ -1,6 +1,289 @@
 import csv
 from neo4j import GraphDatabase
-from py2neo import Graph, Node, Relationship
+from py2neo import Graph, Node, Relationship, Database, NodeMatcher
 
-driver = GraphDatabase.driver(uri="bolt://localhost:7687", auth=("neo4j", "1234"))
-driver.close()
+# graph = Graph()
+# driver = GraphDatabase.driver(uri="bolt://localhost:7687", auth=("neo4j", "1234"), encrypted=False)
+# db=Graph("http://neo4j:1234@127.0.0.1:7474/db/data")
+
+
+# ESTE CUESTIONARIO LO AGREGAMOS AL FINAL
+
+# uri=input("¡Es hora de iniciar! ¿Cual es su URI?\n")
+# user=input("¿Cual es su usuario?\n")
+# password=input("¿Cual es su password?\n")
+db = Graph(uri="bolt://localhost:7687", user="neo4j", password="1234")
+def generateDatabase():
+
+    # we open the csv file and create a dictionary of key = character names and value = list with attributes
+    with open('smashCSV.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        characterDict = {}
+        for row in csv_reader:
+            characterDict[row[0]] = [row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]]
+
+    # we remove the header from the csv file
+    characterDict.pop("Name")
+
+    # here we create our database. we start with the the main connecting nodes
+
+    # fight style
+    brawler = Node("Style", name="Brawler")
+    swordfighter = Node("Style", name="Swordfighter")
+    gunner = Node("Style", name="Gunner")
+
+    # Up b out of shield
+    hasOOS = Node("OOS", name="Has_OOS")
+    noOOS = Node("OOS", name="No_OOS")
+
+    # weight
+    lightweight = Node("Weight", name="Lightweight")
+    mediumweight = Node("Weight", name="Mediumweight")
+    heavyweight = Node("Weight", name="Heavyweight")
+
+    # tier
+    stier = Node("Tier", name="S Tier")
+    atier = Node("Tier", name="A Tier")
+    btier = Node("Tier", name="B Tier")
+    ctier = Node("Tier", name="C Tier")
+    dtier = Node("Tier", name="D Tier")
+
+    # jumps
+    twoJumps = Node("Jumps", name="Two Jumps")
+    threeJumps = Node("Jumps", name="Three Jumps")
+    fourJumps = Node("Jumps", name="Four Jumps")
+    sixJumps = Node("Jumps", name="Six Jumps")
+
+    # sagas
+    starfox = Node("Saga", name="Starfox")
+    bayonetta = Node("Saga", name="Bayonetta")
+    supermariobros = Node("Saga", name="Super Mario Bros")
+    fzero = Node("Saga", name="F-Zero")
+    fireemblem = Node("Saga", name="Fire Emblem")
+    kidicarus = Node("Saga", name="Kid Icarus")
+    donkeykong = Node("Saga", name="Donkey Kong")
+    duckhunt = Node("Saga", name="Duck Hunt")
+    metroid = Node("Saga", name="Metroid")
+    zelda = Node("Saga", name="The Legend of Zelda")
+    pokemon = Node("Saga", name="Pokemon")
+    iceclimbers = Node("Saga", name="Ice Climbers")
+    splatoon = Node("Saga", name="Splatoon")
+    animalcrossing = Node("Saga", name="Animal Crossing")
+    streetfighter = Node("Saga", name="Street Fighter")
+    kingoffighters = Node("Saga", name="King of Fighters")
+    kirby = Node("Saga", name="Kirby")
+    punchout = Node("Saga", name="Punch-Out!")
+    megaman = Node("Saga", name="Mega Man")
+    gameandwatch = Node("Saga", name="Game and Watch")
+    earthbound = Node("Saga", name="Earthbound")
+    pikmin = Node("Saga", name="Pikmin")
+    nes = Node("Saga", name="N.E.S.")
+    castlevania = Node("Saga", name="Castlevania")
+    xenoblade = Node("Saga", name="Xenoblade Chronicles")
+    metalgearsolid = Node("Saga", name="Metal Gear Solid")
+    wiifit = Node("Saga", name="Wii Fit")
+    sonic = Node("Saga", name="Sonic")
+    dragonquest = Node("Saga", name="Dragon Quest")
+    persona = Node("Saga", name="Persona")
+    mii = Node("Saga", name="Mii")
+
+    # Type
+    human = Node("Type", name="Human")
+    fantasy = Node("Type", name="Fantasy")
+    animal = Node("Type", name="Animal")
+
+    # we create the main node relationships: brawler, swordfighter and gunner
+    bs = Relationship(brawler, "connectedTo", swordfighter)
+    db.create(bs)
+    sb = Relationship(swordfighter, "connectedTo", brawler)
+    db.create(sb)
+    bg = Relationship(brawler, "connectedTo", gunner)
+    db.create(bg)
+    gb = Relationship(gunner, "connectedTo", brawler)
+    db.create(gb)
+    sg = Relationship(gunner, "connectedTo", swordfighter)
+    db.create(sg)
+    gs = Relationship(swordfighter, "connectedTo", gunner)
+    db.create(gs)
+
+    # we need to know which category each character belongs to
+    for character in characterDict:
+        if characterDict[character][1] == "Projectile":
+            indicator = "projectile"
+
+        if characterDict[character][1] == "Brawler":
+            indicator = "brawler"
+
+        if characterDict[character][1] == "Sword":
+            indicator = "swordfighter"
+
+        oos = characterDict[character][2]
+        weight = characterDict[character][3]
+        tier = characterDict[character][4]
+        jumps = characterDict[character][5]
+        saga = characterDict[character][6]
+        types = characterDict[character][7]
+
+        # We change the value of the dictionary key to a node of the character with its attributes
+        characterDict[character] = Node("Character", name=characterDict[character][0], style=characterDict[character][1],
+                                        oos=characterDict[character][2], weight=characterDict[character][3],
+                                        tier=characterDict[character][4], jumps=characterDict[character][5],
+                                        saga=characterDict[character][6], types=characterDict[character][7])
+
+        # now we look for the match to put characters on their respective category: brawler, swordfighter, gunner
+        if (indicator == "brawler"):
+            a = Relationship(characterDict[character], "is", brawler)
+            db.create(a)
+        if (indicator == "swordfighter"):
+            a = Relationship(characterDict[character], "is", swordfighter)
+            db.create(a)
+        if (indicator == "projectile"):
+            a = Relationship(characterDict[character], "is", gunner)
+            db.create(a)
+
+        # up b out of shield
+        if (oos == "Yes"):
+            a = Relationship(characterDict[character], "has_oos", hasOOS)
+            db.create(a)
+        if (oos == "No"):
+            a = Relationship(characterDict[character], "has_oos", noOOS)
+            db.create(a)
+
+        # weight
+        if (weight == "Heavy"):
+            a = Relationship(characterDict[character], "weight", heavyweight)
+            db.create(a)
+        if (weight == "Medium"):
+            a = Relationship(characterDict[character], "weight", mediumweight)
+            db.create(a)
+        if (weight == "Light"):
+            a = Relationship(characterDict[character], "weight", lightweight)
+            db.create(a)
+
+        # tier
+        if (tier == "A"):
+            a = Relationship(characterDict[character], "tier", atier)
+            db.create(a)
+        if (tier == "S"):
+            a = Relationship(characterDict[character], "tier", stier)
+            db.create(a)
+        if (tier == "B"):
+            a = Relationship(characterDict[character], "tier", btier)
+            db.create(a)
+        if (tier == "C"):
+            a = Relationship(characterDict[character], "tier", ctier)
+            db.create(a)
+        if (tier == "D"):
+            a = Relationship(characterDict[character], "tier", dtier)
+            db.create(a)
+
+        # jumps
+        if (jumps == "2"):
+            a = Relationship(characterDict[character], "jumps", twoJumps)
+            db.create(a)
+        if (jumps == "3"):
+            a = Relationship(characterDict[character], "jumps", threeJumps)
+            db.create(a)
+        if (jumps == "4"):
+            a = Relationship(characterDict[character], "jumps", fourJumps)
+            db.create(a)
+        if (jumps == "6"):
+            a = Relationship(characterDict[character], "jumps", sixJumps)
+            db.create(a)
+
+        # saga
+        if (saga == "Star Fox"):
+            a = Relationship(characterDict[character], "saga", starfox)
+            db.create(a)
+        if (saga == "Bayonetta"):
+            a = Relationship(characterDict[character], "saga", bayonetta)
+            db.create(a)
+        if (saga == "Super Mario Bros"):
+            a = Relationship(characterDict[character], "saga", supermariobros)
+            db.create(a)
+        if (saga == "F-Zero"):
+            a = Relationship(characterDict[character], "saga", fzero)
+            db.create(a)
+        if (saga == "Fire Emblem"):
+            a = Relationship(characterDict[character], "saga", fireemblem)
+            db.create(a)
+        if (saga == "Kid Icarus"):
+            a = Relationship(characterDict[character], "saga", kidicarus)
+            db.create(a)
+        if (saga == "Donkey Kong"):
+            a = Relationship(characterDict[character], "saga", donkeykong)
+            db.create(a)
+        if (saga == "Duck Hunt"):
+            a = Relationship(characterDict[character], "saga", duckhunt)
+            db.create(a)
+        if (saga == "Metroid"):
+            a = Relationship(characterDict[character], "saga", metroid)
+            db.create(a)
+        if (saga == "The Legend of Zelda"):
+            a = Relationship(characterDict[character], "saga", zelda)
+            db.create(a)
+        if (saga == "Pokemon"):
+            a = Relationship(characterDict[character], "saga", pokemon)
+            db.create(a)
+        if (saga == "Ice Climbers"):
+            a = Relationship(characterDict[character], "saga", iceclimbers)
+            db.create(a)
+        if (saga == "Splatoon"):
+            a = Relationship(characterDict[character], "saga", splatoon)
+            db.create(a)
+        if (saga == "Animal Crossing"):
+            a = Relationship(characterDict[character], "saga", animalcrossing)
+            db.create(a)
+        if (saga == "Street Fighter"):
+            a = Relationship(characterDict[character], "saga", streetfighter)
+            db.create(a)
+        if (saga == "King of Fighters"):
+            a = Relationship(characterDict[character], "saga", kingoffighters)
+            db.create(a)
+        if (saga == "Kirby"):
+            a = Relationship(characterDict[character], "saga", kirby)
+            db.create(a)
+        if (saga == "Punch-Out"):
+            a = Relationship(characterDict[character], "saga", punchout)
+            db.create(a)
+        if (saga == "Mega Man"):
+            a = Relationship(characterDict[character], "saga", megaman)
+            db.create(a)
+        if (saga == "Game & Watch"):
+            a = Relationship(characterDict[character], "saga", gameandwatch)
+            db.create(a)
+        if (saga == "Earthbound"):
+            a = Relationship(characterDict[character], "saga", earthbound)
+            db.create(a)
+        if (saga == "Pikmin"):
+            a = Relationship(characterDict[character], "saga", pikmin)
+            db.create(a)
+        if (saga == "Xenoblade Chronicles"):
+            a = Relationship(characterDict[character], "saga", xenoblade)
+            db.create(a)
+        if (saga == "Metal Gear Solid"):
+            a = Relationship(characterDict[character], "saga", metalgearsolid)
+            db.create(a)
+        if (saga == "Wii Fit"):
+            a = Relationship(characterDict[character], "saga", wiifit)
+            db.create(a)
+        if (saga == "Sonic"):
+            a = Relationship(characterDict[character], "saga", sonic)
+            db.create(a)
+        if (saga == "Dragon Quest"):
+            a = Relationship(characterDict[character], "saga", dragonquest)
+            db.create(a)
+        if (saga == "Persona"):
+            a = Relationship(characterDict[character], "saga", persona)
+            db.create(a)
+        if (saga == "Mii"):
+            a = Relationship(characterDict[character], "saga", mii)
+            db.create(a)
+
+        # we upload them to the database one by one
+        c = characterDict[character]
+        db.create(c)
+
+
+def delete():
+    db.delete_all()
--- Questionaire.py
+++ Questionaire.py
@@ -1,121 +1,228 @@
+# Questions used to get the user preferences
 recommendationQuestions = {
     "player_experience_questions": [
         "He jugado Super Smash Bros antes: \n (1) Si \n (2) No \n>> ",
-        "Tengo experiencia con videojuegos:\n (1) Si \n (2) No",
+        "Tengo experiencia con videojuegos:\n (1) Si \n (2) No\n>> ",
         "Considero que mi nivel en Super Smash Bros. es: \n (1) A penas conozco el juego \n (2) No soy tan bueno \n"
-        " (3) Soy promedio \n (4) Soy bueno \n (5) Jugador profesional"],
+        " (3) Soy promedio \n (4) Soy bueno \n (5) Jugador profesional\n>> "],
 
     "fight_style_questions": [
-        "Preferiría que mi personaje  pelee con: \n (1) Espada \n (2) Cuerpo a cuerpo\n (3) Proyectiles",
+        "Preferiría que mi personaje  pelee con: \n (1) Cuerpo a cuerpo \n (2) Espada \n (3) Proyectiles\n>> ",
         "En los juegos de pelea prefiero: \n (1) Alguien que pelee de cerca \n (2) Alguien que cuente con herramientas"
-        " para luchar \n (3) Un personaje que pueda disparar"],
+        " para luchar \n (3) Un personaje que pueda disparar\n>> "],
 
     "speed_of_play_questions": [
-        "Prefiero un personaje con peso: (1) Pesado \n (2) Normal \n (3) Ligero",
-        "Me gusta poder moverme rapido por el mapa: \n (1) Si \n (2) No",
-        "Me cuesta mantener el ritmo y velocidad: \n (1) Si \n (2) No"],
+        "Prefiero un personaje con peso:\n (1) Pesado \n (2) Normal \n (3) Ligero\n>> ",
+        "Me gusta poder moverme rapido por el mapa: \n (1) Si \n (2) No\n>> ",
+        "Me cuesta mantener el ritmo y velocidad: \n (1) Si \n (2) No\n>> "],
 
     "out_of_shield_questions": [
-        "Utilizo mucho el escudo durante juegos de pelea: (1) Si \n (2) No \n",
-        "Es importante para mi tener ataques rapidos: (1) Si \n (2) No",
-        "Me gustaria tener una manera de atacar rapidamente saliendo del escudo: \n (1) Si \n (2) No"],
+        "Utilizo mucho el escudo durante juegos de pelea:\n (1) Si \n (2) No \n>> ",
+        "Es importante para mi tener ataques rapidos:\n (1) Si \n (2) No\n>> ",
+        "Me gustaria tener una manera de atacar rapidamente saliendo del escudo: \n (1) Si \n (2) No\n>> "],
 
     "tier_questions": [
-        "Se que es un tier en Super Smash Bros: \n (1) Si \n (2) No",
-        "Me importa la clasificacion de nivel de un personaje: \n (1) Si \n (2) No",
+        "Se que es un tier en Super Smash Bros: \n (1) Si \n (2) No\n>> ",
+        "Me importa la clasificacion de nivel de un personaje: \n (1) Si \n (2) No\n>> ",
         "Prefiero que mi personaje sea considerado como (sin importar el nivel del jugador): \n (1) El mejor \n "
-        "(2) Bueno \n (3) Normal \n (4) De los más bajos"],
+        "(2) Bueno \n (3) Normal \n (4) No tan bueno\n (5) De los más bajos\n>> "],
 
     "jump_questions": [
-        "Me cuesta regresar al mapa cuando me caigo: (1) Si \n (2) No",
-        "Prefiero un personaje que tenga: \n (1) Mas de 4 saltos \n (2) Entre 3 y 4 saltos \n (3) 2 saltos"],
+        "Me cuesta regresar al mapa cuando me caigo:\n (1) Si \n (2) No\n>> ",
+        "Prefiero un personaje que tenga:  \n (2) 2 Saltos \n (3) 3 saltos\n (4) 4 saltos\n (5) Mas de 4 saltos\n>> "],
 
     # For inexperienced players, we take into account these questions
 
     "saga_questions": [
-        "Prefiero los videojuegos de: \n (1) Mario \n (2) Pokemon \n (3) Animal Crossing \n (4) Pikmin \n "
-        "(5) Fire Emblem \n (6) Zelda \n (7) Final Fantasy \n (8) Kid Icarus \n (9) Metroid \n "
-        "(10) No conozco estos juegos"],
+        "De los siguientes videojuegos prefiero: \n (1)Starfox           (2)Bayonetta     (3)Super Mario Bros  "
+        "(4)F-Zero           (5)Fire Emblem\n (6)Kid Icarus        (7)Donkey Kong   (8)Duck Hunt         "
+        "(9)Metroid          (10)The Legend of Zelda\n (11)Pokemon          (12)Ice Climbers (13)Splatoon         "
+        "(14)Animal Crossing (15)Street Fighter\n (16)King of Fighters (17)Kirby        (18)Punch-Out!       "
+        "(19)Mega Man        (20)Game and Watch\n (21)Earthbound       (22)Pikmin       (23)N.E.S"
+        "            (24)Castlevania     (25)Xenoblade Chronicles\n (26)Metal Gear Solid (27)Wii Fit      (28)Sonic    "
+        "        (29)Dragon Quest    (30)Persona\n (31)Mii              (32)No conozco estos juegos\n>> "],
 
     "character_type": [
-        "Me importa el diseño de mi personaje: \n (1) Si \n (2) No",
-        "Me gustan las cosas de fantasia: \n (1) Si \n (2) No",
-        "Me gustan los animales: \n (1) Si \n (2) No",
-        "Prefiero jugar con personajes que son: \n (1) Humanos \n (2) Fantasticos \n (3) Animales"]
+        "Me importa el diseño de mi personaje: \n (1) Si \n (2) No\n>> ",
+        "Me gustan las cosas de fantasia: \n (1) Si \n (2) No\n>> ",
+        "Me gustan los animales: \n (1) Si \n (2) No\n>> ",
+        "Prefiero jugar con personajes que son: \n (1) Humanos \n (2) Animales \n (3) Fantásticos\n>> "]
 }
 
+# User preferences used to generate a user node in the database
+user_preferences = {"experience": None, "fight_style": None, "speed_and_weight": None, "out_of_shield": None,
+                    "tier": None, "jump": None, "saga": None, "character_type": None}
 
-def getAnswers(question_dictionary: dict, question_type: str):
-    # Gets the answer and compounds it
-    question_list = question_dictionary.get(question_type)
-    if question_type.lower() == "player_experience_questions":
-        playerExperience(question_list)
-    elif question_type.lower() == "fight_style_questions":
-        fightStyle(question_list)
+saga_dictionary = {1: "Starfox", 2: "Bayonetta", 3: "Super Mario Bros", 4: "F-Zero", 5: "Fire Emblem", 6: "Kid Icarus",
+                   7: "Donkey Kong", 8: "Duck Hunt", 9: "Metroid", 10: "The Legend of Zelda"}
+
+
+def getAnswers(question_dictionary: dict):
+    keys = question_dictionary.keys()
+    for question_type in keys:
+        # Gets the answer and compounds it
+        question_list = question_dictionary.get(question_type)
+        if question_type.lower() == "player_experience_questions":
+            playerExperience(question_list)
+        elif question_type.lower() == "fight_style_questions":
+            fightStyle(question_list)
+        elif question_type.lower() == "speed_of_play_questions":
+            speedOfPlay(question_list)
+        elif question_type.lower() == "out_of_shield_questions":
+            outOfShield(question_list)
+        elif question_type.lower() == "tier_questions":
+            tier(question_list)
+        elif question_type.lower() == "jump_questions":
+            jumps(question_list)
+        elif question_type.lower() == "saga_questions":
+            saga(question_list)
+        elif question_type.lower() == "character_type":
+            characterType(question_list)
+        else:
+            print("No es un atributo valido para clasificar personajes ")
+    return user_preferences
 
 
 def playerExperience(question_list: list):
     total_points = 0
-    print("En esta seccion de preguntas, veremos tu nivel en el juego")
-
-    user_selection = -1
+    print("En esta seccion de preguntas, veremos tu nivel en el juego\n")
 
     # Question 1
-    while not verifyInput(user_selection, 1, 2):
-        try:
-            user_selection = int(input(question_list[0]))
-        except:
-            continue
-
-        twoOptionInput(user_selection, total_points, 5)  # Question with a weight of 5
-    user_selection = -1  # Restart the selection
+    total_points = twoOptionInput(total_points, 5, question_list[0])  # Question with a weight of 5
 
     # Question 2
-    while not verifyInput(user_selection, 1, 2):
-        try:
-            user_selection = int(input(question_list[1]))
-        except:
-            continue
-
-        twoOptionInput(user_selection, total_points, 2)  # Question with a weight of 2
-    user_selection = -1
+    total_points = twoOptionInput(total_points, 1,  question_list[1])  # Question with a weight of 1
 
     # Question 3
-    while not verifyInput(user_selection, 1, 5):
-        try:
-            user_selection = int(input(question_list[2]))
-        except:
-            continue
+    total_points = multipleOptionInput(total_points, 1, question_list[2], 1, 5)  # Question with a weight of 1
 
-        multipleOptionInput(user_selection, total_points, 1)  # Question with a weight of 1
-
-    if total_points <= 11:
-        print("Pro")
-        # TODO add attribute of pro player
-    elif total_points <= 5:
-        print("Casual")
-        # TODO add attribute of casual player
+    if total_points >= 10:
+        user_preferences["experience"] = "pro"
+    elif total_points >= 5:
+        user_preferences["experience"] = "casual"
     else:
-        print("New in the game")
-        # TODO add attribute of new player
-
-
+        user_preferences["experience"] = "new"
 
 
 def fightStyle(question_list: list):
-    user_input = -1
-    point_total = 0
-    weight = 5
-    index = 0
-    while not verifyInput(user_input, 1, 3):
-        try:
-            user_input = int(input(question_list[index]))
-        except Exception:
-            continue
+    total_points = 0
+    print("En esta seccion, estudiaremos tu estilo de pelea\n")
 
-        multipleOptionInput(user_input, point_total, weight)
-        index += 1
-    question_list[0]
+    # Question 1
+    total_points = multipleOptionInput(total_points, 5, question_list[0], 1, 3)  # Question with a weight of 5
+
+    # Question 2
+    total_points = multipleOptionInput(total_points, 3, question_list[1], 1, 3)  # Question with a weight of 3
+
+    if total_points >= 19:
+        user_preferences["fight_style"] = "projectiles"
+    elif total_points >= 9:
+        user_preferences["fight_style"] = "sword"
+    else:
+        user_preferences["fight_style"] = "brawler"
+
+
+def speedOfPlay(question_list: list):
+    total_points = 0
+
+    # Question 1
+    total_points = multipleOptionInput(total_points, 5, question_list[0], 1, 3)  # Weight of 5
+    # Question 2
+    total_points = twoOptionInput(total_points, 3, question_list[1])  # Weight of 3
+    # Question 3
+    total_points = twoOptionInput(total_points, 5, question_list[2])  # Weight of 5
+
+    if total_points >= 14:
+        user_preferences["speed_and_weight"] = "light"
+    elif total_points >= 6:
+        user_preferences["speed_and_weight"] = "normal"
+    else:
+        user_preferences["speed_and_weight"] = "heavy"
+
+
+def outOfShield(question_list: list):
+    total_points = 0
+
+    # Question 1
+    total_points = twoOptionInput(total_points, 2, question_list[0])
+    # Question 2
+    total_points = twoOptionInput(total_points, 5, question_list[1])
+    # Question 3
+    total_points = twoOptionInput(total_points, 7, question_list[2])
+
+    if total_points >= 7:
+        user_preferences["out_of_shield"] = True
+    else:
+        user_preferences["out_of_shield"] = False
+
+
+def tier(question_list: list):
+    total_points = 0
+    # Question 1
+    total_points = twoOptionInput(total_points, 0, question_list[0])  # Weight 0, to put the user in context
+    # Question 2
+    total_points = twoOptionInput(total_points, 100, question_list[1])  # Weight 100
+    # Question 3
+    total_points = multipleOptionInput(total_points, 10, question_list[2], 1, 5)  # Weight 10
+
+    if total_points == 110:
+        user_preferences["tier"] = "s"
+    elif total_points == 120:
+        user_preferences["tier"] = "a"
+    elif total_points == 130:
+        user_preferences["tier"] = "b"
+    elif total_points == 140:
+        user_preferences["tier"] = "c"
+    elif total_points == 150:
+        user_preferences["tier"] = "d"
+
+
+def jumps(question_list: list):
+    total_points = 0
+
+    # Question 1
+    total_points = twoOptionInput(total_points, 10, question_list[0])
+
+    # Question 2
+    total_points = multipleOptionInput(total_points, 10, question_list[1], 2, 5)
+
+    if total_points >= 50:
+        user_preferences["jump"] = 6
+    elif total_points >= 40:
+        user_preferences["jump"] = 4
+    elif total_points >= 30:
+        user_preferences["jump"] = 3
+    else:
+        user_preferences["jump"] = 2
+
+
+def saga(question_list: list):
+    total_points = multipleOptionInput(0, 1, question_list[0], 1, 32)
+    if total_points != 32:
+        user_preferences["saga"] = total_points
+
+
+def characterType(question_list: list):
+    total_points = 0
+
+    # Question 1
+    total_points = twoOptionInput(total_points, 100, question_list[0])  # Weight 100
+
+    # Question 2
+    total_points = twoOptionInput(total_points, 10, question_list[1])  # Weight 10
+
+    # Question 3
+    total_points = twoOptionInput(total_points, 10, question_list[2])  # Weight 10
+
+    # Question 4
+    total_points = multipleOptionInput(total_points, 20, question_list[3], 1, 3)  # Weight 20
+
+    if total_points >= 160:
+        user_preferences["character_type"] = "fantastic"
+    elif total_points >= 140:
+        user_preferences["character_type"] = "animal"
+    elif total_points > 100:
+        user_preferences["character_type"] = "human"
 
 
 def verifyInput(user_input: int, minimum: int, maximum: int):
@@ -125,10 +232,26 @@ def verifyInput(user_input: int, minimum: int, maximum: int):
         return False
 
 
-def twoOptionInput(user_input: int, point_total: int, weight: int):
+def twoOptionInput(point_total: int, weight: int, question: str):
+    user_input = -1
+    while not verifyInput(user_input, 1, 2):
+        try:
+            user_input = int(input(question))
+        except:
+            continue
     if user_input == 1:
         point_total += (1 * weight)
+    return point_total
 
 
-def multipleOptionInput(user_input: int, point_total: int, weight: int):
+def multipleOptionInput(point_total: int, weight: int, question: str, min_value: int, max_value: int):
+    user_input = -1
+    while not verifyInput(user_input, min_value, max_value):
+        try:
+            user_input = int(input(question))
+        except Exception:
+            continue
+
     point_total += (user_input * weight)
+    return point_total
+
