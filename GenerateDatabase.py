adb0c4968641d5d042eb7a5edbff96fc8a27377d
Alejandro Alvarez V <59927580+alejandroav93@users.noreply.github.com>
2020-05-30 22:19:58 -0600

    GenerateDatabase updated version + comments

--- GenerateDatabase.py
+++ GenerateDatabase.py
@@ -9,8 +9,81 @@ db = Graph(uri="bolt://localhost:7687", user="neo4j", password="1234")
 
 # Node matcher
 matcher = NodeMatcher(db)
+# here we create our database. we start with the the main connecting nodes
 
+# fight style
+brawler = Node("Style", name="Brawler")
+swordfighter = Node("Style", name="Swordfighter")
+gunner = Node("Style", name="Gunner")
 
+# Up b out of shield
+hasOOS = Node("OOS", name="Has_OOS")
+noOOS = Node("OOS", name="No_OOS")
+
+# weight
+lightweight = Node("Weight", name="Lightweight")
+mediumweight = Node("Weight", name="Mediumweight")
+heavyweight = Node("Weight", name="Heavyweight")
+
+# tier
+stier = Node("Tier", name="S Tier")
+atier = Node("Tier", name="A Tier")
+btier = Node("Tier", name="B Tier")
+ctier = Node("Tier", name="C Tier")
+dtier = Node("Tier", name="D Tier")
+
+# jumps
+twoJumps = Node("Jumps", name="Two Jumps")
+threeJumps = Node("Jumps", name="Three Jumps")
+fourJumps = Node("Jumps", name="Four Jumps")
+sixJumps = Node("Jumps", name="Six Jumps")
+
+# sagas
+starfox = Node("Saga", name="Starfox")
+bayonetta = Node("Saga", name="Bayonetta")
+supermariobros = Node("Saga", name="Super Mario Bros")
+fzero = Node("Saga", name="F-Zero")
+fireemblem = Node("Saga", name="Fire Emblem")
+kidicarus = Node("Saga", name="Kid Icarus")
+donkeykong = Node("Saga", name="Donkey Kong")
+duckhunt = Node("Saga", name="Duck Hunt")
+metroid = Node("Saga", name="Metroid")
+zelda = Node("Saga", name="The Legend of Zelda")
+pokemon = Node("Saga", name="Pokemon")
+iceclimbers = Node("Saga", name="Ice Climbers")
+splatoon = Node("Saga", name="Splatoon")
+animalcrossing = Node("Saga", name="Animal Crossing")
+streetfighter = Node("Saga", name="Street Fighter")
+kingoffighters = Node("Saga", name="King of Fighters")
+kirby = Node("Saga", name="Kirby")
+punchout = Node("Saga", name="Punch-Out!")
+megaman = Node("Saga", name="Mega Man")
+gameandwatch = Node("Saga", name="Game and Watch")
+earthbound = Node("Saga", name="Earthbound")
+pikmin = Node("Saga", name="Pikmin")
+nes = Node("Saga", name="N.E.S.")
+castlevania = Node("Saga", name="Castlevania")
+xenoblade = Node("Saga", name="Xenoblade Chronicles")
+metalgearsolid = Node("Saga", name="Metal Gear Solid")
+wiifit = Node("Saga", name="Wii Fit")
+sonic = Node("Saga", name="Sonic")
+dragonquest = Node("Saga", name="Dragon Quest")
+persona = Node("Saga", name="Persona")
+mii = Node("Saga", name="Mii")
+
+# Type
+human = Node("Type", name="Human")
+fantasy = Node("Type", name="Fantasy")
+animal = Node("Type", name="Animal")
+
+user_list = []
+
+"""Gnerates Neo4j database for Smash Bros. Main recommendation system
+Args:
+    **
+Returns:
+    **
+"""
 def generateDatabase():
     # we open the csv file and create a dictionary of key = character names and value = list with attributes
     with open('smashCSV.csv') as csv_file:
@@ -23,12 +96,18 @@ def generateDatabase():
     characterDict.pop("Name")
 
     # we create the main node relationships: brawler, swordfighter and gunner
-    db.create(Relationship(brawler, "connectedTo", swordfighter))
-    db.create(Relationship(swordfighter, "connectedTo", brawler))
-    db.create(Relationship(brawler, "connectedTo", gunner))
-    db.create(Relationship(gunner, "connectedTo", brawler))
-    db.create(Relationship(gunner, "connectedTo", swordfighter))
-    db.create(Relationship(swordfighter, "connectedTo", gunner))
+    bs = Relationship(brawler, "connectedTo", swordfighter)
+    db.create(bs)
+    sb = Relationship(swordfighter, "connectedTo", brawler)
+    db.create(sb)
+    bg = Relationship(brawler, "connectedTo", gunner)
+    db.create(bg)
+    gb = Relationship(gunner, "connectedTo", brawler)
+    db.create(gb)
+    sg = Relationship(gunner, "connectedTo", swordfighter)
+    db.create(sg)
+    gs = Relationship(swordfighter, "connectedTo", gunner)
+    db.create(gs)
 
     # we need to know which category each character belongs to
     for character in characterDict:
@@ -57,55 +136,75 @@ def generateDatabase():
 
         # now we look for the match to put characters on their respective category: brawler, swordfighter, gunner
         if indicator == "brawler":
-            db.create(Relationship(characterDict[character], "is", brawler))
+            a = Relationship(characterDict[character], "is", brawler)
+            db.create(a)
         if indicator == "swordfighter":
-            db.create(Relationship(characterDict[character], "is", swordfighter))
+            a = Relationship(characterDict[character], "is", swordfighter)
+            db.create(a)
         if indicator == "projectile":
-            db.create(Relationship(characterDict[character], "is", gunner))
+            a = Relationship(characterDict[character], "is", gunner)
+            db.create(a)
 
         # up b out of shield
         if oos == "Yes":
-            db.create(Relationship(characterDict[character], "has_oos", hasOOS))
+            a = Relationship(characterDict[character], "has_oos", hasOOS)
+            db.create(a)
         if oos == "No":
-            db.create(Relationship(characterDict[character], "has_oos", noOOS))
+            a = Relationship(characterDict[character], "has_oos", noOOS)
+            db.create(a)
 
         # weight
         if weight == "Heavy":
-            db.create(Relationship(characterDict[character], "weight", heavyweight))
+            a = Relationship(characterDict[character], "weight", heavyweight)
+            db.create(a)
         if weight == "Normal":
-            db.create(Relationship(characterDict[character], "weight", mediumweight))
+            a = Relationship(characterDict[character], "weight", mediumweight)
+            db.create(a)
         if weight == "Light":
-            db.create(Relationship(characterDict[character], "weight", lightweight))
+            a = Relationship(characterDict[character], "weight", lightweight)
+            db.create(a)
 
         # tier
         if tier == "A":
-            db.create(Relationship(characterDict[character], "tier", atier))
+            a = Relationship(characterDict[character], "tier", atier)
+            db.create(a)
         if tier == "S":
-            db.create(Relationship(characterDict[character], "tier", stier))
+            a = Relationship(characterDict[character], "tier", stier)
+            db.create(a)
         if tier == "B":
-            db.create(Relationship(characterDict[character], "tier", btier))
+            a = Relationship(characterDict[character], "tier", btier)
+            db.create(a)
         if tier == "C":
-            db.create(Relationship(characterDict[character], "tier", ctier))
+            a = Relationship(characterDict[character], "tier", ctier)
+            db.create(a)
         if tier == "D":
-            db.create(Relationship(characterDict[character], "tier", dtier))
+            a = Relationship(characterDict[character], "tier", dtier)
+            db.create(a)
 
         # jumps
         if jumps == "2":
-            db.create(Relationship(characterDict[character], "jumps", twoJumps))
+            a = Relationship(characterDict[character], "jumps", twoJumps)
+            db.create(a)
         if jumps == "3":
-            db.create(Relationship(characterDict[character], "jumps", threeJumps))
+            a = Relationship(characterDict[character], "jumps", threeJumps)
+            db.create(a)
         if jumps == "4":
-            db.create(Relationship(characterDict[character], "jumps", fourJumps))
+            a = Relationship(characterDict[character], "jumps", fourJumps)
+            db.create(a)
         if jumps == "6":
-            db.create(Relationship(characterDict[character], "jumps", sixJumps))
+            a = Relationship(characterDict[character], "jumps", sixJumps)
+            db.create(a)
 
         # type
         if types == "Human":
-            db.create(Relationship(characterDict[character], "type", human))
+            a = Relationship(characterDict[character], "type", human)
+            db.create(a)
         if types == "Fantasy":
-            db.create(Relationship(characterDict[character], "type", fantasy))
+            a = Relationship(characterDict[character], "type", fantasy)
+            db.create(a)
         if types == "Animal":
-            db.create(Relationship(characterDict[character], "type", animal))
+            a = Relationship(characterDict[character], "type", animal)
+            db.create(a)
 
         # saga
         if saga == "Star Fox":
@@ -175,7 +274,12 @@ def generateDatabase():
         c = characterDict[character]
         db.create(c)
 
-
+"""Creates a characters/saga/attributes in Neo4j database
+Args:
+    *user_preferences (dict)
+Returns:
+    **
+"""
 def generateUser(user_preferences: dict):
     user = Node("User", experience=user_preferences["experience"], style=user_preferences["fight_style"],
                 weight=user_preferences["speed_and_weight"], oos=user_preferences["out_of_shield"],
@@ -299,74 +403,6 @@ def generateUser(user_preferences: dict):
         db.create(Relationship(user, "saga", mii))
 
 
+# Deletes everything from Neo4jDatabase
 def delete():
     db.delete_all()
-
-# Connecting nodes for the database
-
-# fight style
-brawler = Node("Style", name="Brawler")
-swordfighter = Node("Style", name="Swordfighter")
-gunner = Node("Style", name="Gunner")
-
-# Up b out of shield
-hasOOS = Node("OOS", name="Has_OOS")
-noOOS = Node("OOS", name="No_OOS")
-
-# weight
-lightweight = Node("Weight", name="Lightweight")
-mediumweight = Node("Weight", name="Mediumweight")
-heavyweight = Node("Weight", name="Heavyweight")
-
-# tier
-stier = Node("Tier", name="S Tier")
-atier = Node("Tier", name="A Tier")
-btier = Node("Tier", name="B Tier")
-ctier = Node("Tier", name="C Tier")
-dtier = Node("Tier", name="D Tier")
-
-# jumps
-twoJumps = Node("Jumps", name="Two Jumps")
-threeJumps = Node("Jumps", name="Three Jumps")
-fourJumps = Node("Jumps", name="Four Jumps")
-sixJumps = Node("Jumps", name="Six Jumps")
-
-# sagas
-starfox = Node("Saga", name="Starfox")
-bayonetta = Node("Saga", name="Bayonetta")
-supermariobros = Node("Saga", name="Super Mario Bros")
-fzero = Node("Saga", name="F-Zero")
-fireemblem = Node("Saga", name="Fire Emblem")
-kidicarus = Node("Saga", name="Kid Icarus")
-donkeykong = Node("Saga", name="Donkey Kong")
-duckhunt = Node("Saga", name="Duck Hunt")
-metroid = Node("Saga", name="Metroid")
-zelda = Node("Saga", name="The Legend of Zelda")
-pokemon = Node("Saga", name="Pokemon")
-iceclimbers = Node("Saga", name="Ice Climbers")
-splatoon = Node("Saga", name="Splatoon")
-animalcrossing = Node("Saga", name="Animal Crossing")
-streetfighter = Node("Saga", name="Street Fighter")
-kingoffighters = Node("Saga", name="King of Fighters")
-kirby = Node("Saga", name="Kirby")
-punchout = Node("Saga", name="Punch-Out!")
-megaman = Node("Saga", name="Mega Man")
-gameandwatch = Node("Saga", name="Game and Watch")
-earthbound = Node("Saga", name="Earthbound")
-pikmin = Node("Saga", name="Pikmin")
-nes = Node("Saga", name="N.E.S.")
-castlevania = Node("Saga", name="Castlevania")
-xenoblade = Node("Saga", name="Xenoblade Chronicles")
-metalgearsolid = Node("Saga", name="Metal Gear Solid")
-wiifit = Node("Saga", name="Wii Fit")
-sonic = Node("Saga", name="Sonic")
-dragonquest = Node("Saga", name="Dragon Quest")
-persona = Node("Saga", name="Persona")
-mii = Node("Saga", name="Mii")
-
-# Type
-human = Node("Type", name="Human")
-fantasy = Node("Type", name="Fantasy")
-animal = Node("Type", name="Animal")
-
-user_list = []
